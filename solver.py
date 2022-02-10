8c85e27444be28f6b1eef6ef5bb18e97b8ac5cdc
Shail Desai <shail4221@gmail.com>
2022-02-09 16:14:09 -0800

    Added regex processing for last iteration

--- solver.py
+++ solver.py
@@ -1,11 +1,26 @@
 import json
+import operator
+from queue import Empty
+import re
 
 score_dict = {'E':26, 'S':25, 'A':24, 'O':23, 'R':22, 'I':21, 'L':20, 'T':19, 
                 'N':18, 'U':17, 'D':16, 'M':15, 'Y':14, 'C':13, 'P':12, 'H':11,
                 'G':10, 'B':9, 'K':8, 'F':7, 'W':6, 'V':5, 'Z':4, 'X':3,
                 'J':2, 'Q':1}
 
+def score_words(wordlist):
+    scored_words = {}
 
+    for word in wordlist:
+        adjusted_word = ''.join(sorted(set(word), key=word.index))
+        word_score = 0
+        for letter in adjusted_word:
+            word_score += score_dict[letter]
+        scored_words[word] = word_score
+
+    sorted_scored_words = dict(sorted(scored_words.items(), key=operator.itemgetter(1),reverse=True))
+    return list(unaltered_word_dictionary.keys())
+    
 
 with open('Resources/scored_wordlist.txt') as f:
     data = f.read()
@@ -20,6 +35,7 @@ unaltered_word_dictionary = json.loads(data)
 
 print("Please enter Wordle output as a combination of N,G, and Y. N is for a Grey letter, G is for a Green letter, and Y is for a Yellow letter")
 
+unaltered_word_list = list(unaltered_word_dictionary.keys())
 updated_word_list = list(unaltered_word_dictionary.keys())
 
 next_word = updated_word_list[0]
@@ -27,14 +43,28 @@ next_word = updated_word_list[0]
 yellow_letters = []
 grey_letters = []
 green_letters = []
-word_model = ['?', '?', '?', '?', '?']
+word_model = ['[A-Z]', '[A-Z]', '[A-Z]', '[A-Z]', '[A-Z]']
+
+iteration = 0
 
 while(next_word != "no next word"):
+    iteration += 1
+
+    if(iteration == 6):
+        regex_expression = re.compile(''.join(word_model))
+        print(regex_expression)
+        new_list = [x for x in unaltered_word_list if regex_expression.match(x)]
+        print(new_list)
+        if(new_list is not Empty):
+            print("The final guess is " + new_list[0])
+        else:
+            print("The program was not able to find a suitable word (sry)")
+        break
+
     print("Try " + next_word)
 
-    reponse = str(input('What was the wordle output?'))
+    reponse = str(input('What was the wordle output?')).upper()
     
-
     if(reponse == "exit pls"):
         break
 
@@ -53,8 +83,8 @@ while(next_word != "no next word"):
                 yellow_letters.append(next_word[i])
             elif(reponse[i] == 'G'):
                 green_letters.append(next_word[i])
-                word_model[i] = next_word[i]
-                if('?' not in word_model):
+                word_model[i] = "[" + next_word[i] + "]"
+                if('[A-Z]' not in word_model):
                     print("The wordle word for today is " + ''.join(word_model))
                     break
             else:
