337ed22aec9729d76a5914e4b296fb529eeaa63b
Shail Desai <shail4221@gmail.com>
2022-02-09 16:35:07 -0800

    Making yellow letters lower priority when suggesting words

--- solver.py
+++ solver.py
@@ -2,24 +2,29 @@ import json
 import operator
 from queue import Empty
 import re
+from turtle import update
 
 score_dict = {'E':26, 'S':25, 'A':24, 'O':23, 'R':22, 'I':21, 'L':20, 'T':19, 
                 'N':18, 'U':17, 'D':16, 'M':15, 'Y':14, 'C':13, 'P':12, 'H':11,
                 'G':10, 'B':9, 'K':8, 'F':7, 'W':6, 'V':5, 'Z':4, 'X':3,
                 'J':2, 'Q':1}
 
-def score_words(wordlist):
+yellow_score_dict = score_dict
+
+iteration_wordlists = []
+
+def score_words(wordlist, scores):
     scored_words = {}
 
     for word in wordlist:
         adjusted_word = ''.join(sorted(set(word), key=word.index))
         word_score = 0
         for letter in adjusted_word:
-            word_score += score_dict[letter]
+            word_score += scores[letter]
         scored_words[word] = word_score
 
     sorted_scored_words = dict(sorted(scored_words.items(), key=operator.itemgetter(1),reverse=True))
-    return list(unaltered_word_dictionary.keys())
+    return list(sorted_scored_words.keys())
     
 
 with open('Resources/scored_wordlist.txt') as f:
@@ -37,6 +42,7 @@ print("Please enter Wordle output as a combination of N,G, and Y. N is for a Gre
 
 unaltered_word_list = list(unaltered_word_dictionary.keys())
 updated_word_list = list(unaltered_word_dictionary.keys())
+iteration_wordlists.append(updated_word_list)
 
 next_word = updated_word_list[0]
 
@@ -49,6 +55,8 @@ iteration = 0
 
 while(next_word != "no next word"):
     iteration += 1
+
+
 
     if(iteration == 6):
         regex_expression = re.compile(''.join(word_model))
@@ -85,10 +93,9 @@ while(next_word != "no next word"):
             if(reponse[i] == 'N'):
                 grey_letters.append(next_word[i])
                 updated_word_list = [x for x in updated_word_list if next_word[i] not in x]
-                print(updated_word_list)
-                print(len(updated_word_list))
             elif(reponse[i] == 'Y'):
                 yellow_letters.append(next_word[i])
+                yellow_score_dict[next_word[i]] = 0
             elif(reponse[i] == 'G'):
                 green_letters.append(next_word[i])
                 word_model[i] = "[" + next_word[i] + "]"
@@ -98,7 +105,10 @@ while(next_word != "no next word"):
             else:
                 print("Invalid response, exiting")
                 break
-
+            updated_word_list = score_words(updated_word_list, yellow_score_dict)
+            print(updated_word_list)
+            print(len(updated_word_list))
+            iteration_wordlists.append(updated_word_list)
         print("one rotation complete, word model at the moment is " + ''.join(word_model))
 
         if(len(updated_word_list) == 0):
